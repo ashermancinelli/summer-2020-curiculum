9d68180c307e9d05ef862ed63565cb8d0b669e60
Asher Mancinelli <ashermancinelli@gmail.com>
2020-05-03 18:39:58 -0700

    added svm classifier example

--- 03-classification/classify.py
+++ 03-classification/classify.py
@@ -10,4 +10,39 @@ kinds of flowers. We will create a few models which will
 infer the kind of flower from the flower's qualities.
 '''
 import sklearn
-iris_dataset = sklearn.datasets.load_iris()
+from sklearn import datasets
+from sklearn.utils import shuffle
+iris_dataset = datasets.load_iris()
+x = iris_dataset['data']
+y = iris_dataset['target']
+
+# Randomize data
+x, y = shuffle(x, y)
+
+def print_results(predictions, actual):
+    print('Prediction\tActual')
+    print('-' * 30)
+    for i, j in zip(predictions, test_y):
+        print(iris_dataset['target_names'][i],
+              '\t',
+              iris_dataset['target_names'][j])
+
+    failures = sum(predictions - test_y != 0)
+    print(f'\nTotal failures: {failures}')
+    print(f'Accuracy: {failures / len(predictions)}')
+
+if __name__ == '__main__':
+    # Grab 80% of data for training
+    train_x = x[:int(len(x)*.8)]
+    train_y = y[:int(len(y)*.8)]
+
+    # Use the rest for testing
+    test_x = x[int(len(x)*.8):]
+    test_y = y[int(len(y)*.8):]
+
+    from sklearn import svm
+    clf = svm.SVC(gamma=0.001, C=100.)
+    clf.fit(train_x, train_y)
+    predictions = clf.predict(test_x)
+
+    print_results(predictions, test_y)
